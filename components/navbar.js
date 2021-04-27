1bb1056e5dc7011c0888ae8dc6093f9edc33a121
jai-sharma <sendtojsharma@gmail.com>
2021-04-27 07:45:54 +0530

    added navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,30 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button, HStack, Image, Input, Spacer } from '@chakra-ui/react';
+import Container from './container';
 
 export default function Navbar() {
     return (
-        <Box>
-            
+        <Box py="2" boxShadow="md">
+            <Container>
+                <HStack spacing={5}>
+                    <Image src="/assets/logo.svg" />
+                    <Input
+                        placeholder="Search..."
+                        maxW="26rem"
+                        borderColor="#b5bdc4"
+                        borderRadius="5px"
+                    />
+                    <Spacer />
+                    <Button colorScheme="blue">Write a post</Button>
+                    <Image src="/assets/notification.svg" />
+                    <Image src="/assets/bell.svg" />
+                    <Image
+                        w="8"
+                        src="/assets/profile.jpeg"
+                        borderRadius="full"
+                        border="2px solid #eef0f1"
+                    />
+                </HStack>
+            </Container>
         </Box>
-    )
+    );
 }
